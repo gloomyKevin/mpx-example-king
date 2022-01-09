fdb6c2dd6d4b958ca6b4da27ded8f04ab21fccf1
wanyixuan <wanyixuan@didichuxing.com>
2022-01-09 11:52:37 +0800

    feat: migrate argv parse func & extract option

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
-#!/usr/bin/env node
-const fs = require('fs/promises')
-const shell = require('shelljs')
-const path = require('path')
-const execCli = require('./lib/cliExpand')
-const { Logger } = require('./lib/util/index')
-const autoImportSubPackageStyle = require('./lib/processSubpackage')
+// #!/usr/bin/env node
+// const fs = require('fs/promises')
+// const shell = require('shelljs')
+// const path = require('path')
+// const execCli = require('./lib/cliExpand')
+// const { Logger } = require('./lib/util/index')
+// const autoImportSubPackageStyle = require('./lib/processSubpackage')
 const getMergedConfig = require('./lib/resolveConfig')
+const { specificArgsObj, restArgs } = require('./lib/resolveCliArgs')
 
 // 读取config并挂载到全局
 // resolveConfig()
@@ -15,109 +16,109 @@ const getFinalConfig = async () => {
   const mergedConfig = await getMergedConfig()
   console.log('%c [ mergedConfig ]-16', 'font-size:13px; background:pink; color:#bf2c9f;', mergedConfig)
   // const { specificArgsObj } = getSpecificArgsObj()
-  // const finalConfig = Object.assign(mergedConfig, specificArgsObj)
-  // return finalConfig
+  const finalConfig = Object.assign(mergedConfig, specificArgsObj)
+  return finalConfig
 }
+getFinalConfig()
+// // function mountFinalCfgToGlobal () {
+// //   global.getFinalConfig()
+// // }
+// // mountFinalCfgToGlobal()
 
-// function mountFinalCfgToGlobal () {
-//   global.getFinalConfig()
+// // finalConfig 示例
+// const finalConfig = {
+//   // 样式隔离 styleIsolation 开启样式隔离
+//   SWITCH_STYLE_ISOLATION: 'apply-shared',
+//   // map 存储 app.json 中的分包
+//   subPackageMap: new Map(),
+//   // 小程序所在目录
+//   // 小程序文件目录,此处以scripts作为相对，有问题，待修改
+//   miniprogramPath: '../dist/wx',
+//   // 'tailwindcss' 'windicss'
+//   classMode: 'tailwindcss',
+//   // 样式生成策略
+//   cssMode: {
+//     mainPackage: true,
+//     subPackage: true,
+//     specSubPackage: []
+//   },
+//   // 以下为可选，合并策略待定
+//   configPath: ''
 // }
-// mountFinalCfgToGlobal()
+// const miniprogramAbsPath = path.resolve(__dirname, finalConfig.miniprogramPath)
 
-// finalConfig 示例
-const finalConfig = {
-  // 样式隔离 styleIsolation 开启样式隔离
-  SWITCH_STYLE_ISOLATION: 'apply-shared',
-  // map 存储 app.json 中的分包
-  subPackageMap: new Map(),
-  // 小程序所在目录
-  // 小程序文件目录,此处以scripts作为相对，有问题，待修改
-  miniprogramPath: '../dist/wx',
-  // 'tailwindcss' 'windicss'
-  classMode: 'tailwindcss',
-  // 样式生成策略
-  cssMode: {
-    mainPackage: true,
-    subPackage: true,
-    specSubPackage: []
-  },
-  // 以下为可选，合并策略待定
-  configPath: ''
-}
-const miniprogramAbsPath = path.resolve(__dirname, finalConfig.miniprogramPath)
+// // 通过app.json构建map并挂载到全局
+// const setSubpackageMap = async () => {
+//   try {
+//     const data = await fs.readFile(path.resolve(miniprogramAbsPath, './app.json'), 'utf8')
+//     const jsonObject = JSON.parse(data)
+//     const subPackages = jsonObject.subPackages
+//     for (let i = 0, len = subPackages.length; i < len; i++) {
+//       let item = subPackages[i]
+//       let root = item.root
+//       const resolveSubPackagePath = path.resolve(miniprogramAbsPath, root)
+//       finalConfig.subPackageMap.set(resolveSubPackagePath, item)
+//     }
+//   } catch (err) {
+//     throw err
+//   }
+// }
 
-// 通过app.json构建map并挂载到全局
-const setSubpackageMap = async () => {
-  try {
-    const data = await fs.readFile(path.resolve(miniprogramAbsPath, './app.json'), 'utf8')
-    const jsonObject = JSON.parse(data)
-    const subPackages = jsonObject.subPackages
-    for (let i = 0, len = subPackages.length; i < len; i++) {
-      let item = subPackages[i]
-      let root = item.root
-      const resolveSubPackagePath = path.resolve(miniprogramAbsPath, root)
-      finalConfig.subPackageMap.set(resolveSubPackagePath, item)
-    }
-  } catch (err) {
-    throw err
-  }
-}
+// // classMode策略
+// // 输出当前策略提示
 
-// classMode策略
-// 输出当前策略提示
+// // 以下为tailwind的build模式
+// // tailwindBuild()
+// // 扫描
+// /**
+//  * 文件扫描方法
+//  * @param currentPath 当前扫描路径
+//  * @returns {Promise<void>}
+//  */
+// // TODO 使用fast-glob或者node-glob加速扫描
+// const recursiveScanFiles = async currentPath => {
+//   try {
+//     const currentFiles = await fs.readdir(currentPath)
+//     for (let i = 0, len = currentFiles.length; i < len; i++) {
+//       const currentFile = currentFiles[i]
+//       // 绝对路径
+//       const currentAbsPath = path.resolve(currentPath, currentFile)
+//       const currentAbsStat = await fs.stat(currentAbsPath)
+//       const isDirectory = currentAbsStat.isDirectory()
+//       // 设置分包映射 map
+//       await setSubpackageMap()
+//       if (finalConfig.subPackageMap.has(currentAbsPath)) {
+//         // 分包路径下创建相关页面
+//         // 输出 index.wxss 到分包 root
+//         // TODO 增加cssMode判断，改变扫描执行策略及outputPath生成策略
+//         const outputPath = path.resolve(currentPath, currentAbsPath, './index.wxss')
+//         // TODO 不支持npx时直接执行文件
+//         if (!shell.which('npx')) {
+//           // Logger.error('sorry, this script requires npx, please update npm version!')
+//           shell.exit(1)
+//         }
+//         // only for test
+//         // let res = setPresetCfgContent(finalConfig.subPackageMap.get(currentAbsPath).root)
+//         // createContent(res)
+//         execCli(finalConfig, outputPath)
+//         // 自动导入分包样式
+//         await autoImportSubPackageStyle(currentAbsPath, outputPath)
+//       } else if (isDirectory) {
+//         // 深度扫描
+//         await recursiveScanFiles(currentAbsPath)
+//       }
+//     }
+//   } catch (err) {
+//     throw err
+//   }
+// }
 
-// 以下为tailwind的build模式
-// tailwindBuild()
-// 扫描
-/**
- * 文件扫描方法
- * @param currentPath 当前扫描路径
- * @returns {Promise<void>}
- */
-// TODO 使用fast-glob或者node-glob加速扫描
-const recursiveScanFiles = async currentPath => {
-  try {
-    const currentFiles = await fs.readdir(currentPath)
-    for (let i = 0, len = currentFiles.length; i < len; i++) {
-      const currentFile = currentFiles[i]
-      // 绝对路径
-      const currentAbsPath = path.resolve(currentPath, currentFile)
-      const currentAbsStat = await fs.stat(currentAbsPath)
-      const isDirectory = currentAbsStat.isDirectory()
-      // 设置分包映射 map
-      await setSubpackageMap()
-      if (finalConfig.subPackageMap.has(currentAbsPath)) {
-        // 分包路径下创建相关页面
-        // 输出 index.wxss 到分包 root
-        // TODO 增加cssMode判断，改变扫描执行策略及outputPath生成策略
-        const outputPath = path.resolve(currentPath, currentAbsPath, './index.wxss')
-        // TODO 不支持npx时直接执行文件
-        if (!shell.which('npx')) {
-          // Logger.error('sorry, this script requires npx, please update npm version!')
-          shell.exit(1)
-        }
-        // only for test
-        // let res = setPresetCfgContent(finalConfig.subPackageMap.get(currentAbsPath).root)
-        // createContent(res)
-        execCli(finalConfig, outputPath)
-        // 自动导入分包样式
-        await autoImportSubPackageStyle(currentAbsPath, outputPath)
-      } else if (isDirectory) {
-        // 深度扫描
-        await recursiveScanFiles(currentAbsPath)
-      }
-    }
-  } catch (err) {
-    throw err
-  }
-}
+// async function init () {
+//   Logger.warning('==========tailwind compile start==========')
+//   console.time('tailwind build time')
+//   await recursiveScanFiles(miniprogramAbsPath)
+//   Logger.warning('==========tailwind compile end==========')
+//   console.timeEnd('tailwind build time')
+// }
 
-async function init () {
-  Logger.warning('==========tailwind compile start==========')
-  console.time('tailwind build time')
-  await recursiveScanFiles(miniprogramAbsPath)
-  Logger.warning('==========tailwind compile end==========')
-  console.timeEnd('tailwind build time')
-}
-
-init()
+// init()
--- scripts/lib/resolveCliArgs.js
+++ scripts/lib/resolveCliArgs.js
@@ -1,1 +1,161 @@
-function getSpecificArgsObj () {}
+const arg = require('arg')
+// tailwind中 需要从argv中提出并删除的option项
+const tailwindToBeExtractedOptions = ['--output']
+let specificCommands = {
+  // init: {
+  //   args: {
+  //     '--full': { type: Boolean, description: 'Initialize a full `tailwind.config.js` file' },
+  //     '--postcss': { type: Boolean, description: 'Initialize a `postcss.config.js` file' },
+  //     '-f': '--full',
+  //     '-p': '--postcss'
+  //   }
+  // },
+  build: {
+    args: {
+      '--input': { type: String, description: 'Input file' },
+      '--output': { type: String, description: 'Output file' },
+      // '--watch': { type: Boolean, description: 'Watch for changes and rebuild as needed' },
+      // '--content': {
+      //   type: String,
+      //   description: 'Content paths to use for removing unused classes',
+      // },
+      // '--purge': {
+      //   type: String,
+      //   deprecated: true
+      // },
+      // '--postcss': {
+      //   type: oneOf(String, Boolean),
+      //   description: 'Load custom PostCSS configuration'
+      // },
+      // '--minify': { type: Boolean, description: 'Minify the output' },
+      '--config': {
+        type: String,
+        description: 'Path to a custom config file'
+      },
+      // '--no-autoprefixer': {
+      //   type: Boolean,
+      //   description: 'Disable autoprefixer'
+      // },
+      '-c': '--config',
+      '-i': '--input',
+      '-o': '--output',
+      '-m': '--minify',
+      '-w': '--watch'
+    }
+  }
+}
+
+let specificArgsObj = {}
+let restArgs = []
+function getSpecificArgsObj (tailwindArgsObj) {
+  let specificArgs = []
+  Object.entries(tailwindArgsObj).map((cur) => {
+    if (tailwindToBeExtractedOptions.indexOf(cur[0]) > -1) {
+      specificArgs.push(cur)
+    }
+  })
+  specificArgsObj = Object.fromEntries(specificArgs)
+  return {
+    specificArgsObj,
+    restArgs
+  }
+}
+
+let command = ((arg = '') => (arg.startsWith('-') ? undefined : arg))(process.argv[2]) || 'build'
+
+// // if (specificCommands[command] === undefined) {
+// //   if (fs.existsSync(path.resolve(command))) {
+// //     // TODO: Deprecate this in future versions
+// //     // Check if non-existing command, might be a file.
+// //     command = 'build'
+// //   } else {
+// //     help({
+// //       message: `Invalid command: ${command}`,
+// //       usage: ['tailwindcss <command> [options]'],
+// //       commands: Object.keys(commands)
+// //         .filter((command) => command !== 'build')
+// //         .map((command) => `${command} [options]`),
+// //       options: sharedFlags,
+// //     })
+// //     process.exit(1)
+// //   }
+// // }
+
+// // Execute command
+let { args: flags } = specificCommands[command]
+
+const parseCliArgs = (() => {
+  try {
+    let result = arg(
+      Object.fromEntries(
+        Object.entries({ ...flags })
+          .filter(([_key, value]) => !value?.type?.manualParsing)
+          .map(([key, value]) => {
+            return [key, typeof value === 'object' ? value.type : value]
+          })
+      ),
+      { permissive: true }
+    )
+
+    // Manual parsing of flags to allow for special flags like oneOf(Boolean, String)
+    for (let i = result['_'].length - 1; i >= 0; --i) {
+      let flag = result['_'][i]
+      if (!flag.startsWith('-')) continue
+
+      let flagName = flag
+      let handler = flags[flag]
+
+      // Resolve flagName & handler
+      while (typeof handler === 'string') {
+        flagName = handler
+        handler = flags[handler]
+      }
+
+      if (!handler) continue
+
+      let args = []
+      let offset = i + 1
+
+      // Parse args for current flag
+      while (result['_'][offset] && !result['_'][offset].startsWith('-')) {
+        args.push(result['_'][offset++])
+      }
+
+      // Cleanup manually parsed flags + args
+      result['_'].splice(i, 1 + args.length)
+
+      // Set the resolved value in the `result` object
+      result[flagName] = handler.type(
+        args.length === 0 ? undefined : args.length === 1 ? args[0] : args,
+        flagName
+      )
+    }
+
+    // // Ensure that the `command` is always the first argument in the `args`.
+    // // This is important so that we don't have to check if a default command
+    // // (build) was used or not from within each plugin.
+    // //
+    // // E.g.: tailwindcss input.css -> _: ['build', 'input.css']
+    // // E.g.: tailwindcss build input.css -> _: ['build', 'input.css']
+    if (result['_'][0] !== command) {
+      result['_'].unshift(command)
+    }
+    return result
+  } catch (err) {
+    // if (err.code === 'ARG_UNKNOWN_OPTION') {
+    //   help({
+    //     message: err.message,
+    //     usage: ['tailwindcss <command> [options]'],
+    //     options: sharedFlags,
+    //   })
+    //   process.exit(1)
+    // }
+    // throw err
+  }
+})
+getSpecificArgsObj(parseCliArgs())
+
+module.exports = {
+  specificArgsObj,
+  restArgs
+}
